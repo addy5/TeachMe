b87cffd25f006f579cb7d9f5645f805f212f98e8
Addison Chen <addison.k.chen@gmail.com>
2015-07-27 18:33:37 -0700

    added contact form, new user page, WPI in sessions for current_user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,11 +4,23 @@ class UsersController < ApplicationController
   end
 
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new user_params
+    if @user.save
+      redirect_to "/"
+    else
+      render "new"
+    end
   end
 
   def edit
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
   root 'posts#index'
   get '/about' => 'users#about'
   resources :messages, only: [:new, :create]
